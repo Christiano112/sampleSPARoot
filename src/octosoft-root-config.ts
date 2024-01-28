e34817c9889ab6261fa99842e7a32f3ee2243401
christiano112 <enyiachristian12@gmail.com>
2024-01-28 18:24:43 +0100

    added styleguide

--- src/octosoft-root-config.ts
+++ src/octosoft-root-config.ts
@@ -13,9 +13,23 @@ const applications = constructApplications({
     return System.import(name);
   },
 });
-const layoutEngine = constructLayoutEngine({ routes, applications });
+
+// setting active to false means that the app will not be loaded initially until it is activated
+const layoutEngine = constructLayoutEngine({
+  routes,
+  applications,
+  active: false,
+});
 
 applications.forEach(registerApplication);
-layoutEngine.activate();
 
-start();
+System.import("@octosoft/styleguide")
+  .then(() => {
+    // console.log("Imported styleguide");
+    // Activate the layout engine once the styleguide CSS is loaded
+    layoutEngine.activate();
+    start();
+  })
+  .catch((e) => {
+    // console.error(e, "Error loading styleguide");
+  });
